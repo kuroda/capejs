7489b8375df41759b1abea774aeae1593b6e9c68
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-07-14 09:10:51 +0900

    CollectionAgent [continue]

--- test/spec/collection_agent_test.js
+++ test/spec/collection_agent_test.js
@@ -3,18 +3,16 @@
 describe('CollectionAgent', function() {
   describe('constructor', function() {
     it('should take resource name as the first argument', function() {
-      var form, agent;
+      var agent;
 
-      form = { id: 123 };
       agent = new Cape.CollectionAgent('users');
 
       expect(agent.resourceName).to.equal('users');
     })
 
     it('should take an object (options) as the second argument', function() {
-      var form, options, agent;
+      var options, agent;
 
-      form = { id: 123 };
       options = { pathPrefix: '/api/' }
       agent = new Cape.CollectionAgent('users', options);
 
@@ -22,17 +20,53 @@ describe('CollectionAgent', function() {
     })
 
     it('should call agent adapter', function() {
-      var form, options, agent;
+      var options, agent;
 
       Cape.AgentAdapters.FooBarAdapter = sinon.spy();
 
-      form = { id: 123 };
       options = { adapter: 'foo_bar' }
       new Cape.CollectionAgent('user', options);
 
       expect(Cape.AgentAdapters.FooBarAdapter.called).to.be.true;
 
       Cape.AgentAdapters.FooBarAdapter = undefined;
+    })
+  })
+
+
+  describe('#collectionPath', function() {
+    it('should return standard values', function() {
+      var agent;
+
+      agent = new Cape.CollectionAgent('users');
+
+      expect(agent.collectionPath()).to.equal('/users');
+    })
+
+    it('should add prefix to the paths', function() {
+      var agent;
+
+      agent = new Cape.CollectionAgent('users', { pathPrefix: '/api/' });
+
+      expect(agent.collectionPath()).to.equal('/api/users');
+    })
+  })
+
+  describe('#memberPath', function() {
+    it('should return standard values', function() {
+      var agent;
+
+      agent = new Cape.CollectionAgent('users');
+
+      expect(agent.memberPath(123)).to.equal('/users/123');
+    })
+
+    it('should add prefix to the paths', function() {
+      var agent;
+
+      agent = new Cape.CollectionAgent('users', { pathPrefix: '/api/' });
+
+      expect(agent.memberPath(123)).to.equal('/api/users/123');
     })
   })
 })
